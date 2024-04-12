66040d575d091f33ae067a447af4c6446ab0c6ec
MaelJEAN <mael.jean@etu.mines-ales.fr>
2024-04-12 16:20:07 +0200

    group, implication, origin, person classes builder

--- address-app/domain/src/main/java/fr/mines/religion/domain/model/Group.java
+++ address-app/domain/src/main/java/fr/mines/religion/domain/model/Group.java
@@ -1,4 +1,30 @@
 package fr.mines.religion.domain.model;
 
-public class Group {
+public record Group(String name, String description) {
+
+    public static final class GroupBuilder {
+        private String name;
+        private String description;
+
+        private GroupBuilder() {
+        }
+
+        public static GroupBuilder aGroup() {
+            return new GroupBuilder();
+        }
+
+        public GroupBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public GroupBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Group build() {
+            return new Group(name, description);
+        }
+    }
 }
--- address-app/domain/src/main/java/fr/mines/religion/domain/model/Person.java
+++ address-app/domain/src/main/java/fr/mines/religion/domain/model/Person.java
@@ -1,6 +1,42 @@
 package fr.mines.religion.domain.model;
 
-import java.util.UUID;
+public record Person(String firstName, String lastName, int age, String gender) {
 
-public record Person(UUID uuid, String firstName, String lastName, Origin origin, Politic politic, Religion religion) {
+    public static final class PersonBuilder {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String gender;
+
+        private PersonBuilder() {
+        }
+
+        public static PersonBuilder aPerson() {
+            return new PersonBuilder();
+        }
+
+        public PersonBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public PersonBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder withAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder withGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(firstName, lastName, age, gender);
+        }
+    }
 }
